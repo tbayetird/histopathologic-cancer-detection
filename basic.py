668f6dd357003fb71de806549c6dd7591a68ee5d
Ahmad Fall <fallahmad@outlook.com>
2019-03-06 16:37:40 +0000

    Updated config files, added dataset setup util

--- basic.py
+++ basic.py
@@ -1,9 +1,10 @@
+from config import config
+from imutils import paths
 import os
-
 import utils.create_models
-local_dir = os.path.abspath('.')
-mock_data_dir = os.path.join(local_dir,'mock_data')
-model_path='' # no model to load yet
+from keras.preprocessing.image import ImageDataGenerator
+
+batch_size = 32
 
 # Set the image size.
 img_height = 96
@@ -13,16 +14,39 @@ img_width = 96
 # LOADING MODEL
 ###############################
 
-
-model = utils.create_models.create_mlp((96,96,3))
+model = utils.create_models.create_mlp((img_width,img_height,3))
 model.summary()
 
 ################################
 # PREPROCESSING DATAS
 ###############################
 
+train_datagen = ImageDataGenerator(preprocessing_function=lambda x:(x - x.mean()) / x.std() if x.std() > 0 else x,
+                                   rescale=1./255,
+                                   width_shift_range=4,
+                                   height_shift_range=4,
+                                   rotation_range=90,
+                                   zoom_range=0.2, 
+                                   horizontal_flip=True,
+                                   vertical_flip=True)
 
-#Nothing yet
+test_datagen = ImageDataGenerator(preprocessing_function=lambda x:(x - x.mean()) / x.std() if x.std() > 0 else x, rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+    config.TRAIN_PATH,
+    class_mode="categorical",
+    target_size=(img_width, img_height),
+    color_mode="rgb",
+    shuffle=True,
+    batch_size=batch_size)
+
+valid_generator = test_datagen.flow_from_directory(
+    config.VAL_PATH,
+    class_mode="categorical",
+    target_size=(img_width, img_height),
+    color_mode="rgb",
+    shuffle=False,
+    batch_size=batch_size)
 
 
 ################################
