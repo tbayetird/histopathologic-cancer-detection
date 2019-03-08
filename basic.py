10ca238aaf16ca2bfa3efe75d8fbbe35aaaffc86
tbayetird <theophile.bayet@ird.fr>
2019-03-08 10:52:25 +0000

    ajout des graphes

--- basic.py
+++ basic.py
@@ -15,6 +15,7 @@ import shutil
 from skimage.io import imread
 import numpy as np
 from time import gmtime, strftime
+import matplotlib.pyplot as plt
 
 ################################
 # SETTING UP DATASET
@@ -105,7 +106,27 @@ history = model.fit_generator(
 # TRAINING VISUALS
 ###############################
 
-# Nothing yet
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+
+epochs = range(1, len(acc) +1)
+
+plt.plot(epochs,acc,'bo',label='Training acc')
+plt.plot(epochs,val_acc,'b',label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+plt.plot(epochs,loss,'bo',label='Training loss')
+plt.plot(epochs,val_loss,'b',label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
+
 
 #%%
 ################################
